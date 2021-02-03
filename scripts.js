d3d841f44276df7bb3ef0e27a4eed3cc4db3a7f6
Rafael Zorzenon <rafaasimi@hotmail.com>
2021-02-03 13:18:15 -0300

    Criação dos scripts

---
+++ scripts.js
@@ -0,0 +1,195 @@
+// Abrir e fechar Modal
+const Modal = {
+  open() {
+    document.querySelector(".modal-overlay").classList.add("active");
+  },
+  close() {
+    document.querySelector(".modal-overlay").classList.remove("active");
+  },
+};
+
+// Mostrar/Esconder valores de entradas/saidas/total
+const visibilityEye = document.querySelector("#visibilityEye");
+const cardValues = document.querySelectorAll(".card p");
+const dataIncomeValues = document.querySelectorAll(".income");
+const dataExpenseValues = document.querySelectorAll(".expense");
+let visibilityState = true;
+
+const visibilityValues = {
+  toggle() {
+    if (visibilityState) {
+      cardValues.forEach((value) => {
+        value.style.filter = `blur(5px)`;
+      });
+      // dataIncomeValues.forEach((value) => {
+      //     value.style.filter = `blur(5px)`
+      // })
+      // dataExpenseValues.forEach((value) => {
+      //     value.style.filter = `blur(5px)`
+      // })
+      visibilityEye.src = "./assets/eye-hidden.png";
+      visibilityState = false;
+    } else {
+      cardValues.forEach((value) => {
+        value.style.filter = `blur(0px)`;
+      });
+      // dataIncomeValues.forEach((value) => {
+      //     value.style.filter = `blur(0px)`
+      // })
+      // dataExpenseValues.forEach((value) => {
+      //     value.style.filter = `blur(0px)`
+      // })
+      visibilityEye.src = "./assets/eye-visible.png";
+      visibilityState = true;
+    }
+  },
+};
+
+// Inserir ano atual no footer
+document.querySelector("#current-year").innerText = new Date().getFullYear();
+
+
+// Calculos das transações
+const Transaction = {
+  all: [
+    {
+      description: "Luz",
+      amount: -50000,
+      date: "01/02/2021",
+    },
+    {
+      description: "Criação website",
+      amount: 500000,
+      date: "01/02/2021",
+    },
+    {
+      description: "Internet",
+      amount: -20050,
+      date: "01/02/2021",
+    },
+    {
+      description: "App",
+      amount: 300000,
+      date: "01/02/2021",
+    },
+  ],
+
+  add(transaction) {
+    Transaction.all.push(transaction);
+    App.reload();
+  },
+
+  remove(index) {
+    Transaction.all.splice(index, 1);
+    App.reload();
+  },
+
+  incomes() {
+    let income = 0;
+    // Somar todas as entradas
+    Transaction.all.forEach((transaction) => {
+      transaction.amount > 0 ? (income += transaction.amount) : income;
+    });
+
+    return income;
+  },
+
+  expenses() {
+    let expense = 0;
+    // Somar todas as saídas
+    Transaction.all.forEach((transaction) => {
+      transaction.amount < 0 ? (expense += transaction.amount) : expense;
+    });
+
+    return expense;
+  },
+
+  total() {
+    // Calcular o total de entradas-saídas
+    return Transaction.incomes() + Transaction.expenses();
+  },
+};
+
+// Formatação de Moeda
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+    value = String(value).replace(/\D/g, "");
+    value = Number(value) / 100;
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+    return signal + value;
+  },
+};
+
+const Form = {
+    submit(event) {
+        
+
+        
+        Modal.close();
+    }
+}
+
+// Coloca os dados das transações no HTML
+const DOM = {
+  transactionsContainer: document.querySelector("#data-table tbody"),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    DOM.transactionsContainer.appendChild(tr);
+  },
+
+  innerHTMLTransaction({ description, amount, date }) {
+    const CSSClass = amount > 0 ? "income" : "expense";
+
+    amount = Utils.formatCurrency(amount);
+
+    const html = `
+            <td class="description">${description}</td>
+            <td class="${CSSClass}">${amount}</td>
+            <td class="date">${date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+        `;
+    return html;
+  },
+
+  updateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    );
+    document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    );
+    document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.total()
+    );
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = "";
+  },
+};
+
+const App = {
+    init() {
+
+        Transaction.all.forEach((transaction) => {
+            DOM.addTransaction(transaction);
+          });
+          
+          DOM.updateBalance();
+
+    },
+    reload() {
+        DOM.clearTransactions();
+        App.init();
+    },
+}
+
+App.init();
